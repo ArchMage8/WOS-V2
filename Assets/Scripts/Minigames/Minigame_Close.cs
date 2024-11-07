41dc1b89043178e916a83b84e457f56c3405e434
Darryl Orleans <darryl.orleans@binus.ac.id>
2024-11-07 16:07:46 +0700

    Update Minigame_Close.cs

--- Assets/Scripts/Minigames/Minigame_Close.cs
+++ Assets/Scripts/Minigames/Minigame_Close.cs
@@ -4,9 +4,42 @@ using UnityEngine;
 
 public class Minigame_Close : MonoBehaviour
 {
+    [Header("Sprite Settings")]
+    public Sprite defaultSprite;
+    public Sprite hoverSprite;
+
     public GameObject TargetMinigame;
     private Animator TargetAnimator;
     private bool canClose = true;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+
+        // Set to default sprite initially
+        if (defaultSprite != null)
+        {
+            spriteRenderer.sprite = defaultSprite;
+        }
+    }
+
+    private void OnMouseEnter()
+    {
+        if (hoverSprite != null)
+        {
+            spriteRenderer.sprite = hoverSprite;
+        }
+    }
+
+    private void OnMouseExit()
+    {
+        if (defaultSprite != null)
+        {
+            spriteRenderer.sprite = defaultSprite;
+        }
+    }
 
     private void OnMouseDown()
     {
